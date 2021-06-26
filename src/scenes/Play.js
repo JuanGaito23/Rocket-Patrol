d15b2890bc48a8f8b931ea52dd219226d6452c3e
--replace-all <jgaito@ucsc.edu>
2021-06-25 19:37:49 -0500

    Final Game Revisions

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -60,14 +60,21 @@ class Play extends Phaser.Scene {
 
     // 60-second play clock
     scoreConfig.fixedWidth = 0;
-    this.clock = this.time.delayedCall(60000, () => {
+    this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
         this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or ← for Menu', scoreConfig).setOrigin(0.5);
         this.gameOver = true;
     }, null, this);
 
     }
     update() {
+        // check key input for restart
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+            this.scene.restart();
+        }
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+            this.scene.start("menuScene");
+        }
         if (!this.gameOver) {               
             this.p1Rocket.update();         // update rocket sprite
             this.ship01.update();           // update spaceships (x3)
